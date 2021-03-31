67b48ba1be9604d8f28cbf50ae0cfc67fb6cd671
DESKTOP-AICEQIT\niels <niels@bosmainteractive.se>
2021-03-31 18:01:55 +0200

    Work

--- Tessin.Bladerunner/Controls/IconButton.cs
+++ Tessin.Bladerunner/Controls/IconButton.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using LINQPad;
 using LINQPad.Controls;
 
 // https://materialdesignicons.com/icon/svg
@@ -9,6 +11,11 @@ namespace Tessin.Bladerunner.Controls
 {
     public class IconButton : Button
     {
+        public static object HorizontalRun(params IconButton[] buttons)
+        {
+            return Util.HorizontalRun(false, buttons.Where(f => f != null).ToList());
+        }
+
         public IconButton(string icon, Action<Button> onClick, string tooltip = "") : base("", onClick)
         {
             this.HtmlElement.SetAttribute("class", "icon-button");
