a02f70a336479ae60da8c6909171e429a77509e6
Niels Bosma <niels@tessin.se>
2021-10-25 16:45:59 +0200

    Work

--- Tessin.Bladerunner/Controls/Card.cs
+++ Tessin.Bladerunner/Controls/Card.cs
@@ -10,10 +10,18 @@ namespace Tessin.Bladerunner.Controls
 {
     public class Card : Div
     {
-        public Card(object content) : base()
+        public Card(object content, string title = null) : base()
         {
             this.SetClass("card");
             var dc = new DumpContainer {Content = content};
+
+            if(!string.IsNullOrEmpty(title))
+            {
+                var titleDiv = new Div(new Literal(title));
+                titleDiv.SetClass("card--title");
+                this.VisualTree.Add(titleDiv);
+            }
+
             this.VisualTree.Add(dc);
         }
     }
